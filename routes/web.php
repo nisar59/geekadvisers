518a55bf2d04de0bbb62b6cf599b99ebbfc4adb4
Mahar Nisar <umair.awan@akhuwat.org.pk>
2022-12-23 22:15:25 +0500

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,25 @@ use Illuminate\Support\Facades\Auth;
 
 
 
-Route::get('/', [App\Http\Controllers\WelcomeController::class, 'welcome'])->name('welcome');
-Route::get('all_notice', [App\Http\Controllers\AllNoticeController::class, 'all_notice'])->name('allNotice');
-Route::get('contact-us', [App\Http\Controllers\ContactController::class, 'contact'])->name('contact.us');
-Route::get('about-us', [App\Http\Controllers\AboutusController::class, 'about_us'])->name('about.us');
-Route::get('disclaimer', [App\Http\Controllers\DisclaimerController::class, 'disclaimer'])->name('disclaimer.page');
-Route::get('privacy-policy', [App\Http\Controllers\PrivacyController::class, 'privacy_policy'])->name('privacy.page');
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'welcome']);
+Route::get('all_notice', [App\Http\Controllers\AllNoticeController::class, 'all_notice']);
+Route::get('contact-us', [App\Http\Controllers\ContactController::class, 'contact']);
+Route::get('about-us', [App\Http\Controllers\AboutusController::class, 'about_us']);
+Route::get('disclaimer', [App\Http\Controllers\DisclaimerController::class, 'disclaimer']);
+Route::get('privacy-policy', [App\Http\Controllers\PrivacyController::class, 'privacy_policy']);
 
 
 /*--===============Menu Route==============--*/
-Route::get('/admin/dashboard/about-us', [App\Http\Controllers\menu\AboutusController::class, 'aboutUs'])->name('aboutUs');
+Route::get('/admin/dashboard/about-us', [App\Http\Controllers\menu\AboutusController::class, 'aboutUs']);
+
 Route::post('/admin/dashboard/about-us', [App\Http\Controllers\menu\AboutusController::class, 'aboutUs_content']);
 
-Route::get('/admin/dashboard/disclaimer', [App\Http\Controllers\menu\DisclaimerController::class, 'disclaimerPage'])->name('disclaimer.admin.page');
+Route::get('/admin/dashboard/disclaimer', [App\Http\Controllers\menu\DisclaimerController::class, 'disclaimerPage']);
+
 Route::post('/admin/dashboard/disclaimer', [App\Http\Controllers\menu\DisclaimerController::class, 'disclaimer_content']);
 
-Route::get('/admin/dashboard/privacy-policy', [App\Http\Controllers\menu\PrivacyController::class, 'privacyPolicy'])->name('privacyPolicy');
+Route::get('/admin/dashboard/privacy-policy', [App\Http\Controllers\menu\PrivacyController::class, 'privacyPolicy']);
+
 Route::post('/admin/dashboard/privacy-policy', [App\Http\Controllers\menu\PrivacyController::class, 'privacy_content']);
 
 
@@ -49,47 +52,61 @@ Route::post('/getgranterUpazila', [App\Http\Controllers\Officer\OfficerControlle
 Route::post('/getgranterUnions', [App\Http\Controllers\Officer\OfficerController::class, 'getgranter_Unions']);
 
 Route::post('/getgranterTwoUpazila', [App\Http\Controllers\Officer\OfficerController::class, 'getgranter_2_Upazila']);
+
 Route::post('/getgranterTwoUnions', [App\Http\Controllers\Officer\OfficerController::class, 'getgranter_2_Unions']);
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
 
 /*--------------Admin Panel Route---------------------*/
-Route::get('/admin/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'dashboardView'])->name('admin.dashboard')->middleware('is_admin');
-Route::get('/admin/change-password', [App\Http\Controllers\Admin\DashboardController::class, 'changePassword_Page'])->name('change.password')->middleware('is_admin');
+Route::get('/admin/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'dashboardView'])->middleware('is_admin');
+
+Route::get('/admin/change-password', [App\Http\Controllers\Admin\DashboardController::class, 'changePassword_Page'])->middleware('is_admin');
+
 Route::post('/admin/change-password', [App\Http\Controllers\Admin\DashboardController::class, 'changePassword_update'])->middleware('is_admin');
-Route::get('/admin/site-settings', [App\Http\Controllers\Admin\SettingsController::class, 'settings_Page'])->name('site.settings')->middleware('is_admin');
+
+Route::get('/admin/site-settings', [App\Http\Controllers\Admin\SettingsController::class, 'settings_Page'])->middleware('is_admin');
+
 Route::post('/admin/site-settings', [App\Http\Controllers\Admin\SettingsController::class, 'settings_Update'])->middleware('is_admin');
-Route::get('/admin/all-notice', [App\Http\Controllers\Admin\NoticeController::class, 'all_notice'])->name('all.notice')->middleware('is_admin');
-Route::get('/admin/loginfo/{id}', [App\Http\Controllers\UserLogController::class, 'index'])->name('user.loginfo')->middleware('is_admin');
+Route::get('/admin/all-notice', [App\Http\Controllers\Admin\NoticeController::class, 'all_notice'])->middleware('is_admin');
+
+Route::get('/admin/loginfo/{id}', [App\Http\Controllers\UserLogController::class, 'index'])->middleware('is_admin');
 
 
 /*-------------------------Admin List---------------------------------*/
-Route::get('/admin/list-admin', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Page'])->name('list.admin')->middleware('is_admin');
-Route::post('/admin/list-admin', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Post'])->name('list.admin');
-Route::get('/admin/list-admin/{id}/edit', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Edit'])->name('list.admin.edit')->middleware('is_admin');
-Route::post('/admin/list-admin/{id}/edit', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Update'])->name('list.admin.edit')->middleware('is_admin');
-Route::get('/admin/list-admin/{id}/delete', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Delete'])->name('list.admin.delete')->middleware('is_admin');
+Route::get('/admin/list-admin', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Page'])->middleware('is_admin');
+
+Route::post('/admin/list-admin', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Post']);
+
+Route::get('/admin/list-admin/{id}/edit', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Edit'])->middleware('is_admin');
+
+Route::post('/admin/list-admin/{id}/edit', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Update'])->middleware('is_admin');
+
+Route::get('/admin/list-admin/{id}/delete', [App\Http\Controllers\Admin\ListAdminController::class, 'listadmin_Delete'])->middleware('is_admin');
 
 /*-----------------Create Notice------------------------------*/
-Route::get('/create-notice', [App\Http\Controllers\Admin\NoticeController::class, 'notice_page'])->name('notice.create');
-Route::post('/create-notice', [App\Http\Controllers\Admin\NoticeController::class, 'notice_create'])->name('notice.create');
-Route::get('/notice-edit/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Edit'])->name('notice.edit');
-Route::post('/notice-edit/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Update'])->name('notice.edit');
-Route::get('/notice-delete/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Delete'])->name('notice.delete');
-Route::get('/admin/dashboard/all-member-list', [App\Http\Controllers\Admin\MemberController::class, 'allMemberList'])->name('all.member.list');
-Route::get('/admin/dashboard/member-edit/list', [App\Http\Controllers\Admin\MemberController::class, 'allEditRequestMember'])->name('all.edit.request.member');
-Route::get('/admin/dashboard/member-edit/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditByAdmin'])->name('member.edit.byadmin');
+Route::get('/create-notice', [App\Http\Controllers\Admin\NoticeController::class, 'notice_page']);
+Route::post('/create-notice', [App\Http\Controllers\Admin\NoticeController::class, 'notice_create']);
+Route::get('/notice-edit/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Edit']);
+Route::post('/notice-edit/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Update']);
+Route::get('/notice-delete/{id}', [App\Http\Controllers\Admin\NoticeController::class, 'notice_Delete']);
+Route::get('/admin/dashboard/all-member-list', [App\Http\Controllers\Admin\MemberController::class, 'allMemberList']);
 
-Route::get('/admin/dashboard/memberedit/request/view/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestView'])->name('member.edit.request.view');
-Route::post('/admin/dashboard/memberedit/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditByAdminPost'])->name('member.edit.by.admin');
+Route::get('/admin/dashboard/member-edit/list', [App\Http\Controllers\Admin\MemberController::class, 'allEditRequestMember']);
 
-Route::get('/admin/dashboard/memberedit/request/aprove/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestAprove'])->name('member.edit.request.aprove');
+Route::get('/admin/dashboard/member-edit/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditByAdmin']);
 
-Route::get('/admin/dashboard/memberedit/request/reject/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestRejected'])->name('member.edit.request.reject');
+Route::get('/admin/dashboard/memberedit/request/view/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestView']);
 
-Route::get('/admin/dashboard/view-member-profile/{id}', [App\Http\Controllers\Admin\MemberController::class, 'MemberView'])->name('member.view.profile');
+Route::post('/admin/dashboard/memberedit/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditByAdminPost']);
 
-Route::get('/admin/dashboard/member-delete/{id}', [App\Http\Controllers\Admin\MemberController::class, 'MemberDelete'])->name('member.delete');
+Route::get('/admin/dashboard/memberedit/request/aprove/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestAprove']);
+
+Route::get('/admin/dashboard/memberedit/request/reject/{id}', [App\Http\Controllers\Admin\MemberController::class, 'loanmember_EditRequestRejected']);
+
+Route::get('/admin/dashboard/view-member-profile/{id}', [App\Http\Controllers\Admin\MemberController::class, 'MemberView']);
+
+Route::get('/admin/dashboard/member-delete/{id}', [App\Http\Controllers\Admin\MemberController::class, 'MemberDelete']);
 
 Route::get('/admin/dashboard/member-loan-approve/{id}', [App\Http\Controllers\Admin\MemberController::class, 'MemberLoanApprove']);
 
